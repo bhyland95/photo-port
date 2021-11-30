207139cd99f262b4490b66c869f7c96d3d8e90e8
Bhyland <blakehyland95@gmail.com>
2021-11-30 14:27:28 -0700

    adding test to current code

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+        render(<About />);
+    });
+    // Second Test
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<About />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
